7548a680abed5996759de53768a1d6ba3264d004
gr1zzly-be4r <jake.bruemmer@gmail.com>
2015-07-21 13:12:39 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple merge sort
+
+Simple implementation of merge sort algorithm just to see what it would be written like in Ruby.
+
+
---
+++ sorter.rb
@@ -0,0 +1,57 @@
+# Simple merge sorting algorithm
+class Sorter
+  
+  def initialize()
+  end
+
+  def m_sort(array)
+    
+    if array.nil? then return end
+
+    length = array.length
+    p "length is #{length}"
+    if length < 2 then return end
+    
+    midpoint = length / 2
+    # Split up the arrays
+    left_array = length % 2 == 0 ? array.slice(0..(midpoint - 1)) : array.slice(0..midpoint)
+    right_array = length % 2 == 0 ? array.slice(midpoint..length) : array.slice((midpoint + 1)..length)
+
+    m_sort(left_array)
+    m_sort(right_array)
+    helper(left_array, right_array, array, left_array.length, right_array.length)
+
+  end
+
+  def helper(left, right, array, left_length, right_length)
+    left_i = 0
+    right_i = 0
+    sorted_i = 0
+
+    while (left_i < left_length && right_i < right_length) do
+      if (left[left_i] < right[right_i])
+        array[sorted_i] = left[left_i]
+        left_i += 1
+        sorted_i += 1
+      else
+        array[sorted_i] = right[right_i]
+        right_i += 1
+        sorted_i += 1
+      end
+    end
+
+    # Take care of the residual stuff
+    while (left_i < left_length) do
+      array[sorted_i] = left[left_i]
+      left_i += 1
+      sorted_i += 1
+    end
+    while (right_i < right_length) do
+      array[sorted_i] = right[right_i]
+      right_i += 1
+      sorted_i += 1
+    end
+
+  end
+end
+
